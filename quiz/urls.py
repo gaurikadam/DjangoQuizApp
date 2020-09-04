5c86806b556b67da122167f0040617df1eb68aeb
Gauri Kadam <gauribkadam@gmail.com>
2020-09-04 10:06:13 +0530

    Auth views addded

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,URLPattern
-
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 # from . import views
 from .views import IndexView,SignUpView,QuizView,ResultView
 
 urlpatterns = [
     # ex: /polls/
+    path('account/login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
     path('', IndexView.as_view(), name='index'),
     path('signup/',SignUpView.as_view(), name='signup'),
     path('questions/<slug:pk>/',QuizView.as_view(),name='questions'),
